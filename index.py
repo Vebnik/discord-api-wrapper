e02625bf61f0ada80187d29925a7f825dfc12cf6
Vebnik(Nik) <vebnik111@gmail.com>
2022-11-12 21:24:25 +0300

    add broken eventloop fir gateway

---
+++ index.py
@@ -0,0 +1,15 @@
+import src.gateway_connect as gtw
+
+
+def main():
+
+  auth_data = {
+
+  }
+
+  gateway = gtw.Gateway('', '', '')
+
+  pass
+
+
+if __name__ == '__main__': main()
--- src/gateway_connect.py
+++ src/gateway_connect.py
@@ -1,7 +1,11 @@
-import requests as req
+import websockets
+import json
+import asyncio
 
 
 class Gateway:
+
+  gateway_url = 'wss://gateway.discord.gg/?v=10&encoding=json'
 
   auth_data = {
     'token': None,
@@ -9,16 +13,33 @@ class Gateway:
     'secret_key': None,
   }
 
-  def __init__(self, token, app_id, secret_key):
+
+  def __init__(self, token: str, app_id: str, secret_key: str):
+
+    print('init gateway')
 
     self.auth_data['token'] = token
     self.auth_data['app_id'] = app_id
     self.auth_data['secret_key'] = secret_key
 
-    self.get_gateway()
+    asyncio.run(self.get_gateway())
     pass
 
 
-  def get_gateway(self):
-    
-    pass
+  async def get_gateway(self) -> None:
+    async with websockets.connect(self.gateway_url) as ws:
+
+      d = 251
+      response = json.loads(await ws.recv())
+      interaval = response['d']['heartbeat_interval']
+      
+      while True:
+        await ws.send(json.dumps({"op": 1,"d": d}))
+        print(await ws.recv())
+
+        await asyncio.sleep(interaval/1000)
+      
+
+
+  
+  
