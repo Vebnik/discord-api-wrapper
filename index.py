8089e6e30853e267854b190d2c7472e724e99e40
Vebnik(Nik) <vebnik111@gmail.com>
2022-11-21 20:16:00 +0300

    add asyncio event loop

--- index.py
+++ index.py
@@ -1,11 +1,20 @@
-import src.auth.gateway_connect as gtw
+import asyncio
+import multiprocessing as mlp
+import src.auth.gateway_connect as Gtw
+import src.event.event_listener as Evt
+
+
+def init_event_listen():
+  Evt.Event_Listen()
+
+
+def init_app():
+  Gtw.Gateway()
 
 
 def main():
-
-  gateway = gtw.Gateway('', '', '')
-
-  pass
+  init_app()
 
 
 if __name__ == '__main__': main()
+
--- src/auth/gateway_connect.py
+++ src/auth/gateway_connect.py
@@ -1,69 +1,82 @@
 import websockets
 import pprint as pp
 import json
-import asyncio
 import datetime as dt
+import asyncio
+import threading as thr
+from time import sleep
 from src.auth.auth_data import *
 
 
 class Gateway:
 
   gateway_url = 'wss://gateway.discord.gg/?v=10&encoding=json'
+  auth_data = { 'token': None, 'app_id': None, 'secret_key': None }
 
-  auth_data = {
-    'token': None,
-    'app_id': None,
-    'secret_key': None,
-  }
+  loop = None
+  ws = None
+  asyncio = None
 
-
-  def __init__(self, token: str, app_id: str, secret_key: str):
+  
+  def __init__(self):
     print('init gateway')
-
-    self.auth_data['token'] = token
-    self.auth_data['app_id'] = app_id
-    self.auth_data['secret_key'] = secret_key
-
     asyncio.run(self.get_gateway())
-    pass
 
 
-  async def first_connect(self, ws, d):
-    print(f'{"-"*20} => {dt.datetime.today().strftime("%H:%M:%S")}\nheartbeat event')
+  async def first_connect(self, d):
 
-    await ws.send(json.dumps({"op": 1,"d": d}))
-    print(await ws.recv())
-    await asyncio.sleep(2)
-
-    return ws
+    await self.ws.send(json.dumps({"op": 1,"d": d}))
+    await self.ws.recv()
+  
+    return self.ws
 
 
-  async def auth_app(self, ws):
-    print(f'{"-"*20} => {dt.datetime.today().strftime("%H-%M-%S")}\nsubscribe event')
+  async def auth_app(self):
+    print(f'{"-"*20} => subscribe event')
 
     buffer = []
 
-    await ws.send(json.dumps(auth_data))
+    await self.ws.send(json.dumps(auth_data))
 
-    buffer.append(await ws.recv())
-    buffer.append(await ws.recv())
+    buffer.append(await self.ws.recv())
+    buffer.append(await self.ws.recv())
     
-    for data in map(lambda el: json.loads(el), buffer): pp.pprint(data)
-    return ws
+    buffer = [*map(lambda el: json.loads(el), buffer)]
 
+    with open('src/data/auth_response.json', 'w+', encoding='utf-8') as file: 
+      file.write(json.dumps(buffer[0], indent=2))
+
+    print('Write nsubscribe data')
+
+    return self.ws
+
+
+  async def heartbeat(self, interval):
+    while True:
+      await asyncio.sleep(interval)
+      await self.ws.send(json.dumps({"op": 1,"d": 0}))
+      print('Hearbeat event')
+
+
+  async def event_listener(self):
+    while True:
+      event = await self.ws.recv()
+      print(event)
+    
 
   async def get_gateway(self) -> None:
 
-    async with websockets.connect(self.gateway_url) as ws:
+    self.ws = await websockets.connect(self.gateway_url)
 
-      response = json.loads(await ws.recv())
+    response = json.loads(await self.ws.recv())
 
-      print(f'First connect data\n{response}')
-      
-      if response['op'] == 10:
-        await self.first_connect(ws, 0)
-        await self.auth_app(ws)
+    print(f'First connect data\n{response}')
+    
+    if response['op'] == 10:
 
-        while True:
-          await self.first_connect(ws, 0)
+      asyncio.ensure_future(self.heartbeat(10))
+
+      await self.first_connect(0)
+      await self.auth_app()
+      await self.event_listener()
 
